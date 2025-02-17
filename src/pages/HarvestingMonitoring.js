9bcd457eafd48a8501d135d115f0adf383a11a08
vsrimal <vishwadeaero@gmail.com>
2025-02-18 01:01:03 +0530

    Minor Update

--- src/pages/HarvestingMonitoring.js
+++ src/pages/HarvestingMonitoring.js
@@ -21,15 +21,15 @@ const HarvestingMonitoring = () => {
 
             <div className="p-6 border rounded-md bg-white shadow-sm">
 
-            <div className="flex justify-end">
+                <div className="flex justify-end">
                     <button onClick={() => {
                         navigate("/harvesting-monitoring/view");
                     }} className="px-2 py-1 w-20 border border-sky-900 bg-slate-50 text-gray-700 rounded-full hover:bg-gray-300">View</button>
                 </div>
 
                 {/* Reusable Harvesting Monitoring Component */}
-                <HarvestingMonitoringForm initialData={null} onSubmit={handleSave} onCancel={() => navigate("/maturation-monitoring/view")} />
-                
+                <HarvestingMonitoringForm initialData={null} onSubmit={handleSave} onCancel={() => navigate("/harvesting-monitoring/view")} />
+
             </div>
         </AppLayout>
     );
